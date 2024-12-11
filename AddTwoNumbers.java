42d2808e5569b82bcf8dc123de265b4d169e8044
DevOps-50 <renuka22550@gmail.com>
2024-12-11 15:49:52 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,18 @@
+public class AddTwoNumbers {
+  public static void main(String[] args) {
+    if(args.length<2) {
+      System.out.println("Please provide two numbers as arguments.");
+      return;
+    }
+    //Parse the numbers from command-line arguments
+  int num1=Integer.parseInt(args[0]);
+  int num2=Integer.parseInt(args[1]);
+
+    //Calculate the sum
+    int sum=num1+num2;
+    //display the result
+    System.out.println("The sum of " + num1 + " and "+ num2 + " is: " + sum);
+  }
+}
+    
+    
